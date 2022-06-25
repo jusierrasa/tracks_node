36122f21482c44dcd55aa03872d3a78e7bcc85a9
Julián Sierra Salamanca <sierrasalamancaj@gmail.com>
2022-06-25 18:54:11 -0500

    automatizando las rutas de las páginas-

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
+/*
+  * Aquí invocamos a las rutas
+*/
+//TODO localhost/api/______
+app.use("/api", require("./routes"))
+
 app.listen(port, () => {
   console.log(`Tu app esta lista por http://localhost: ${port}`);
 });
